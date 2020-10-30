ea68aaf9c0878382e353850ce1c6f7bf23a9ab53
Aliyu Abdullahi <aliyu.olalekan13@gmail.com>
2020-10-30 02:28:18 +0100

    Cache selected First responder for subsequent app launch

--- app/src/main/java/com/lek/rates/RatesApp.kt
+++ app/src/main/java/com/lek/rates/RatesApp.kt
@@ -1,7 +1,18 @@
 package com.lek.rates
 
 import android.app.Application
+import android.content.Context
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-open class RatesApp : Application()
+open class RatesApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        context = this.applicationContext
+    }
+
+    companion object{
+        lateinit var context: Context
+    }
+}
